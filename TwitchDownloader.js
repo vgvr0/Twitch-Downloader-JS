baa4108c4d8307899cd99ebe92e8f4f57df3996b
vgvr0 <126200367+vgvr0@users.noreply.github.com>
2023-06-11 09:57:56 +0200

    Add files via upload

---
+++ TwitchDownloader.js
@@ -0,0 +1,30 @@
+const TwitchDownloader = require('twitch-dl');
+
+// Twitch video ID to download
+const videoId = '123456789';
+
+// Output directory where the downloaded video will be saved
+const outputDir = '/path/to/directory';
+
+// Download options
+const options = {
+  output: `${outputDir}/%(title)s.%(ext)s`, // Output directory and file name format
+};
+
+// Download the video
+const downloader = new TwitchDownloader(videoId, options);
+downloader.getInfo((err, info) => {
+  if (err) {
+    console.error('Error downloading video:', err);
+  } else {
+    console.log('Title:', info.title);
+    console.log('Description:', info.description);
+    downloader.download((err, info) => {
+      if (err) {
+        console.error('Error downloading video:', err);
+      } else {
+        console.log('Video downloaded:', info.file);
+      }
+    });
+  }
+});
